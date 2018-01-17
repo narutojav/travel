d95adf1da1eba942c561ff3e13b4b111ee4c43c4
hishigbat <hishigbat@gmail.com>
2018-01-17 13:39:36 +0800

    12

---
+++ js/script.js
@@ -0,0 +1,26 @@
+var swiper = new Swiper('#slider .swiper-container', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.s-right',
+    prevEl: '.s-left',
+  },
+});
+var topswiper = new Swiper('#top .swiper-container', {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: '#top .swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '#top .s-right',
+    prevEl: '#top .s-left',
+  },
+});
